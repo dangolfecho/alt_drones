8c462704419633bb01fec1fefc6710dbce941d9e
gideon <55153800+dangolfecho@users.noreply.github.com>
2026-09-05 00:13:39 +0530

    Tested a snippet

--- ppo_only/code_testing.py
+++ ppo_only/code_testing.py
@@ -23,65 +23,73 @@ def test(lower_bound, upper_bound, mode, rpy_flag=0):
     vals.append(start[0][rpy_flag])
     start = save.copy()
 
-mode = int(input('enter mode to test\n'))
-if(mode%2):
-    low = 0
-    upper=0
-    prev=0
-    for i in range(19, -1, -1):
-        for j in range(100):
-            if(mode == 0):
-                low = -(i*(3.14/20))
-                upper = i*(3.14/20)
+def runner():
+    mode = int(input('enter mode to test\n'))
+    if(mode%2):
+        low = 0
+        upper=0
+        prev=0
+        for i in range(19, -1, -1):
+            for j in range(100):
+                if(mode == 0):
+                    low = -(i*(3.14/20))
+                    upper = i*(3.14/20)
+                    test(low, upper, mode, 0)
+                elif(mode == 1):
+                    low = i*(3.14/20)
+                    upper = 3.14
+                    test(low, upper, mode, 0)
+                elif(mode == 2):
+                    low = ((i-1)*(3.14/20))
+                    upper = i*(3.14/20)
+                    test(low, upper, mode, 0)
+                elif(mode == 3):
+                    low =(i*(3.14/20))
+                    upper = (i+1)*(3.14/20)
+                    test(low, upper, mode, 0)
+            xs = [0]*(len(vals))
+            title = f'low={low} upper={upper} prev={prev}\
+                mode = {mode}'
+            fig, ax = plt.subplots()
+            fig = plt.scatter(vals, xs)
+            plt.xlim(-3.14, 3.14)
+            plt.ylim(-0.1, 0.1)
+            ax.set_title(title)
+            plt.show()
+            vals.clear()
+    else:
+        low = 0
+        upper=0
+        prev=0
+        for i in range(1, 19):
+            for j in range(100):
+                if(mode == 0):
+                    low = -(i*(3.14/20))
+                    upper = i*(3.14/20)
+                elif(mode == 1):
+                    low = i*(3.14/20)
+                    upper = 3.14
+                elif(mode == 2):
+                    low = ((i-1)*(3.14/20))
+                    upper = i*(3.14/20)
+                elif(mode == 3):
+                    low =(i*(3.14/20))
+                    upper = (i+1)*(3.14/20)
                 test(low, upper, mode, 0)
-            elif(mode == 1):
-                low = i*(3.14/20)
-                upper = 3.14
-                test(low, upper, mode, 0)
-            elif(mode == 2):
-                low = ((i-1)*(3.14/20))
-                upper = i*(3.14/20)
-                test(low, upper, mode, 0)
-            elif(mode == 3):
-                low =(i*(3.14/20))
-                upper = (i+1)*(3.14/20)
-                test(low, upper, mode, 0)
-        xs = [0]*(len(vals))
-        title = f'low={low} upper={upper} prev={prev}\
-            mode = {mode}'
-        fig, ax = plt.subplots()
-        fig = plt.scatter(vals, xs)
-        plt.xlim(-3.14, 3.14)
-        plt.ylim(-0.1, 0.1)
-        ax.set_title(title)
-        plt.show()
-        vals.clear()
-else:
-    low = 0
-    upper=0
-    prev=0
-    for i in range(1, 19):
-        for j in range(100):
-            if(mode == 0):
-                low = -(i*(3.14/20))
-                upper = i*(3.14/20)
-            elif(mode == 1):
-                low = i*(3.14/20)
-                upper = 3.14
-            elif(mode == 2):
-                low = ((i-1)*(3.14/20))
-                upper = i*(3.14/20)
-            elif(mode == 3):
-                low =(i*(3.14/20))
-                upper = (i+1)*(3.14/20)
-            test(low, upper, mode, 0)
-        xs = [0]*(len(vals))
-        title = f'low={low} upper={upper} prev={prev}\
-            mode = {mode}'
-        fig, ax = plt.subplots()
-        fig = plt.scatter(vals, xs)
-        plt.xlim(-3.14, 3.14)
-        plt.ylim(-0.1, 0.1)
-        ax.set_title(title)
-        plt.show()
-        vals.clear()
+            xs = [0]*(len(vals))
+            title = f'low={low} upper={upper} prev={prev}\
+                mode = {mode}'
+            fig, ax = plt.subplots()
+            fig = plt.scatter(vals, xs)
+            plt.xlim(-3.14, 3.14)
+            plt.ylim(-0.1, 0.1)
+            ax.set_title(title)
+            plt.show()
+            vals.clear()
+
+def runner1():
+    a=int(input("Enter 0 or 1"))
+    b = (a == 1)*10
+    print(b)
+
+runner1()
