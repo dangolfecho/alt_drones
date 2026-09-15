5a7cde22eb09f7f4978eac47fb8e02386077ab94
gideon <55153800+dangolfecho@users.noreply.github.com>
2026-09-15 16:12:54 +0530

    Change to script args

--- ppo_only/same_script.sh
+++ ppo_only/same_script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#python learn_changing_orien.py env algo pos_orn_flag flag mode lower_bound upper_bound 
-python learn_changing_orien.py 0 4 0 0 0 -0.0 0.0 0
+#python learn_changing_orien.py env algo adaptive_train_flag pos_orn_flag flag mode lower_bound upper_bound 
+python learn_changing_orien.py 0 4 0 0 0 0 -0.0 0.0 0
